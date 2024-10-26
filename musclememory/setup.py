f0c1e515d39ca8a61062048f88468972a2fdd13b
jjaegii <hn06038@gmail.com>
2024-10-27 00:56:10 +0900

    chore: Add package setup configuration
    - Add package metadata
    - Configure dependencies
    - Add CLI entry point
    - Set up package structure

--- musclememory/setup.py
+++ musclememory/setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='musclememory',
-    version='0.1',
+    name="musclememory",
+    version="0.1",
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'click',
+        'Click',
+        'GitPython',
+        'cryptography>=42.0.0',
     ],
-    entry_points='''
-        [console_scripts]
-        musclememory=musclememory.cli:cli
-    ''',
+    entry_points={
+        'console_scripts': [
+            'muscle=musclememory.cli:cli',
+        ],
+    },
 )
